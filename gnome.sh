81c20a90b3c508d9168d83cf7ddd31a914dce58b
Bernardo Sulzbach <bernardo@bernardosulzbach.com>
2018-09-26 17:45:21 -0300

    Updated Gnome script

--- gnome.sh
+++ gnome.sh
@@ -2,5 +2,5 @@ gsettings set org.gnome.desktop.interface enable-animations false
 
 gsettings set org.gnome.desktop.interface clock-show-seconds true
 
-gsettings set org.gnome.desktop.peripherals.keyboard delay 100
-gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 10
+gsettings set org.gnome.desktop.peripherals.keyboard delay 192
+gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 8
