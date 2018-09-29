a852846925f8b82d3a9701300463ded3e5c9c9f8
Bernardo Sulzbach <bernardo@bernardosulzbach.com>
2018-09-29 15:57:42 -0300

    Should also install PIP packages

--- housekeepeer.py
+++ housekeepeer.py
@@ -1,5 +1,6 @@
 import subprocess
 
+PIP_FILE = 'pip.txt'
 PACKAGES_FILE = 'packages.txt'
 CURRENT_JETBRAINS_VERSION = "2018.2.4"
 
@@ -7,6 +8,7 @@ IDEA_LINK = "https://download.jetbrains.com/idea/ideaIU-{}.tar.gz"
 CLION_LINK = "https://download.jetbrains.com/cpp/CLion-{}.tar.gz"
 PYCHARM_LINK = "https://download.jetbrains.com/python/pycharm-professional-{}.tar.gz"
 DATAGRIP_LINK = "https://download.jetbrains.com/datagrip/datagrip-{}.tar.gz"
+
 
 
 def install_jetbrains_products():
@@ -20,9 +22,16 @@ def install_operating_system_packages():
     print(command)
     subprocess.call(command.split())
 
+def install_python_packages():
+    with open(PIP_FILE) as packages_file:
+        packages = [line.strip() for line in packages_file.readlines()]
+    command = 'pip install --user -r ' + PIP_FILE
+    print(command)
+    subprocess.call(command.split())
 
 def main():
     install_operating_system_packages()
+    install_python_packages()
 
 
 if __name__ == '__main__':
