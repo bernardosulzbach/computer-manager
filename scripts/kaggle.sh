a1607c86f13c8895cc9e14d9b736db07869fd6e3
Bernardo Sulzbach <bernardo@bernardosulzbach.com>
2018-10-02 16:15:18 -0300

    Working Kaggle script

--- scripts/kaggle.sh
+++ scripts/kaggle.sh
@@ -1,1 +1,7 @@
-ln -s /home/bernardo/Dropbox/kaggle/kaggle.json .kaggle.json
+#!/usr/bin/env bash
+
+mkdir -p ~/.kaggle && cd ~/.kaggle || exit
+rm -f .kaggle.json
+ln -s ~/Dropbox/kaggle/kaggle.json .kaggle.json
+echo "Done."
+ls -la ~/.kaggle
