dc6681c69608c7dce8fee3c466a300effcf61aad
Bernardo Sulzbach <bernardo@bernardosulzbach.com>
2019-07-14 23:56:12 -0300

    Should fetch IntelliJ from stable
    Updates #6

--- includes/snap-3.sh
+++ includes/snap-3.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sudo snap install clion --classic --edge
-sudo snap install pycharm-professional --classic --edge
-sudo snap install intellij-idea-ultimate --classic --edge
-sudo snap install webstorm --classic --edge
-sudo snap install datagrip --classic --edge
+sudo snap install clion --classic --channel=stable
+sudo snap install pycharm-professional --classic --channel=stable
+sudo snap install intellij-idea-ultimate --classic --channel=stable
+sudo snap install webstorm --classic --channel=stable
+sudo snap install datagrip --classic --channel=stable
